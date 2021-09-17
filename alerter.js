1f02412dbc7cbea349231c0313281a14c26950d3
Jay <mantine08@gmail.com>
2021-09-17 09:50:40 -0700

    Fixing deployment

--- alerter.js
+++ alerter.js
@@ -14,7 +14,7 @@ const CHANNEL_ID = process.env.CHANNEL_ID;
 bot.once("ready", () => {
   (async () => {
     const browser = await puppeteer.launch({
-      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
     page.setDefaultTimeout(0);
